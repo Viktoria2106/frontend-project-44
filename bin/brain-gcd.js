d470e277527eeb8a8b468738bc183be497c9a360
Victoria <konkova2006@inbox.ru>
2025-06-06 14:12:48 +0300

    test

--- bin/brain-gcd.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import playGcdGame from '../src/games/gcd.js';
-
-playGcdGame();
--- bin/brain-prime.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import playPrimeGame from '../src/games/prime.js';
-
-playPrimeGame();
