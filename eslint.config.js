d9ee29c9f71b1df033a556cac31512cacb3fb499
Viktoria2106 <konkova2006@inbox.ru>
2025-06-09 11:53:04 +0300

    test23

--- eslint.config.js
+++
@@ -1,7 +0,0 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import importPlugin from 'eslint-plugin-import';
-import { FlatCompat } from '@eslint/eslintrc';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
