54ab1f98e07f0eebf205bb294045d23241c1b4c0
Viktoria2106 <konkova2006@inbox.ru>
2025-06-09 19:08:42 +0300

    test33

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,2 @@
-const getRandomNum = (min = 1, max = 99) => Math.round(Math.random() * (max - min)) + min;
-export default getRandomNum;
+const getRandomNum = (min = 1, max = 99) => Math.round(Math.random() * (max - min)) + min
+export default getRandomNum
