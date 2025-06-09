a80e07db7a9f5f9681f987f3b2d06a21d559289e
Viktoria2106 <konkova2006@inbox.ru>
2025-06-09 18:49:51 +0300

    test31

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import run from '../src/games/even.js';
+import run from '../src/games/even.js'
 
-run();
+run()
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import run from '../src/games/prime.js';
+import run from '../src/games/prime.js'
 
-run();
+run()
