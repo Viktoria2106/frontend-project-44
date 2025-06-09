4ed73df3840e62ac7972ba6122f7a04bd7f4d36d
Viktoria2106 <konkova2006@inbox.ru>
2025-06-09 11:45:08 +0300

    test22

--- eslint.config.js
+++ eslint.config.js
@@ -5,12 +5,3 @@ import { FlatCompat } from '@eslint/eslintrc';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended,
-});
-
-export default 
-pluginJs.configs.recommended,
