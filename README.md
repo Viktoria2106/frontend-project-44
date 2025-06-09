1fd0503fe959b1bb4a13bf246bb419516bd38582
Viktoria2106 <konkova2006@inbox.ru>
2025-06-09 14:17:20 +0300

    test25

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/Viktoria2106/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Viktoria2106/frontend-project-44/actions)
 
 <a href="https://codeclimate.com/github/Viktoria2106/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/d529b761f4231e03513d/maintainability" /></a>
+[![Quality gate](https://sonarcloud.io/api/project_badges/quality_gate?project=Viktoria2106_frontend-project-44)](https://sonarcloud.io/summary/new_code?id=Viktoria2106_frontend-project-44)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=Viktoria2106_frontend-project-44&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=Viktoria2106_frontend-project-44)
--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,48 @@
 import js from "@eslint/js";
 import globals from "globals";
+import { fileURLToPath } from 'url';
+import path from 'path';
 import { defineConfig } from "eslint/config";
+import pluginJs from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  pluginJs.configs.recommended,
+    ...compat.extends('airbnb-base'),
+  {
+    languageOptions: {
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+      },
+      globals: {
+        ...globals.jest,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      import: importPlugin
+    },
+    rules: {
+      ...importPlugin.configs.recommended.rules,
+      'no-underscore-dangle': [
+        'error',
+        { allow: ['__filename', '__dirname'] }
+      ],
+      'import/extensions': ['error', { js: 'always' }],
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off'
+    }
+  }
+];
